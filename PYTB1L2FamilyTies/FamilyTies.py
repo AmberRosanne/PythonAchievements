1de0b60a64d5e59550cff070f2f9e078ad22b65f
Amber <AmberRosannee@outlook.com>
2020-10-23 15:55:53 +0200

    Les 2 - 'FamilyTies' gemaakt

---
+++ PYTB1L2FamilyTies/FamilyTies.py
@@ -0,0 +1,16 @@
+Naam = "Amber"
+Leeftijd = 19
+Woonplaats = "Spaarndam"
+AfstandSchool = 17.2
+Hobbies = "Discord, Gamen, Tekenen, Fotografie en Grafisch Ontwerp"
+Geboorteplaats = "Alkmaar"
+Reistijd = 1.5
+Opleiding = "Allround DTP"
+
+print("-----------------------------------------------------------------------------")
+print("Hoi! Mijn naam is " + Naam + " en ik ben" , Leeftijd, "jaar oud.")
+print("Ik ben geboren in " + Geboorteplaats + ", maar ik woon nu in " + Woonplaats + ".")
+print("Ik woon " , AfstandSchool, "KM af van school, maar door de slechte verbinging ben ik " , Reistijd, "uur onderweg met het OV.")
+print("Mijn hobbies zijn: " + Hobbies + ".")
+print("Voor deze opleiding heb ik de opleiding " + Opleiding + " gedaan.")
+print("-----------------------------------------------------------------------------")
