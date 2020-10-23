67bbff0958e1e55f0bf0ee90a2ccce1cb9e441c5
Amber <AmberRosannee@outlook.com>
2020-10-23 21:52:57 +0200

    PY les 3 af?

---
+++ PYTB1L3SchoolTool/SchoolTool.py
@@ -0,0 +1,16 @@
+maandag = True
+dinsdag = True
+woensdag = True
+donderdag = True
+vrijdag = True
+zaterdag = False
+zondag = False
+schooldag = maandag
+
+
+print("Moet je vandaag naar school?")
+
+if schooldag == True:
+    print("Ja, je moet naar school! Pak je spullen en ga!")
+else:
+    print("Nope, geen school vandaag! Lekker uitslapen dus!")
