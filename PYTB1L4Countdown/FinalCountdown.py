1e4695857df4a7d69edc18e0f0d2bb13354ec972
Amber <AmberRosannee@outlook.com>
2020-10-29 12:41:23 +0100

    les 4 af

---
+++ PYTB1L4Countdown/FinalCountdown.py
@@ -0,0 +1,11 @@
+import webbrowser
+import time
+
+for x in reversed(range (1001)):
+    print(x)
+
+
+if x == 0:
+    print("Hier komt een leuke video!")
+    time.sleep(1)
+    webbrowser.open("https://www.youtube.com/watch?v=BjDebmqFRuc", new=0)
